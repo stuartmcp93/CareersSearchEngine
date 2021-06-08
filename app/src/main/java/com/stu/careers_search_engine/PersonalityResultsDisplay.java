ed8b7335055bf3f9832f0c97c5e7b435445a9fe7
Stuart McPherson <stuartmcp93@gmail.com>
2021-06-08 08:26:02 +0100

    Added personality_quiz_results.xml layout.

---
+++ app/src/main/java/com/stu/careers_search_engine/PersonalityResultsDisplay.java
@@ -0,0 +1,71 @@
+package com.stu.careers_search_engine;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
+import java.util.Objects;
+
+public class PersonalityResultsDisplay extends AppCompatActivity {
+    TextView tvR, tvPython, tvCPP, tvJava;
+    PieChart pieChart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personality_quiz_results);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+
+        // Link those objects with their respective
+        // id's that we have given in .XML file
+        tvR = findViewById(R.id.tvR);
+        tvPython = findViewById(R.id.tvPython);
+        tvCPP = findViewById(R.id.tvCPP);
+        tvJava = findViewById(R.id.tvJava);
+        pieChart = findViewById(R.id.piechart);
+
+        // Creating a method setData()
+        // to set the text in text view and pie chart
+        setData();
+    }
+
+    private void setData()
+    {
+
+        // Set the percentage of language used
+        tvR.setText(Integer.toString(40));
+        tvPython.setText(Integer.toString(30));
+        tvCPP.setText(Integer.toString(5));
+        tvJava.setText(Integer.toString(25));
+
+        // Set the data and color to the pie chart
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Conscientiousness",
+                        Integer.parseInt(tvR.getText().toString()),
+                        Color.parseColor("#FFA726")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Python",
+                        Integer.parseInt(tvPython.getText().toString()),
+                        Color.parseColor("#66BB6A")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "C++",
+                        Integer.parseInt(tvCPP.getText().toString()),
+                        Color.parseColor("#EF5350")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Java",
+                        Integer.parseInt(tvJava.getText().toString()),
+                        Color.parseColor("#29B6F6")));
+
+        // To animate the pie chart
+        pieChart.startAnimation();
+    }
+}
