f587129537378a1ea8e01d404498681386b6f413
Stuart McPherson <stuartmcp93@gmail.com>
2021-06-16 09:12:11 +0100

    //Github check 16/06 9:11am

--- app/src/main/java/com/stu/careers_search_engine/Home.java
+++ app/src/main/java/com/stu/careers_search_engine/Home.java
@@ -2,17 +2,47 @@ package com.stu.careers_search_engine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Objects;
 
-public class Home extends AppCompatActivity {
+public class Home extends Activity {
+
+    Button BTN_takeQuiz, BTN_viewResults, BTN_suggestedCareers, BTN_favouritesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
-        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        //Assign values to buttons
+        BTN_takeQuiz = findViewById(R.id.BTN_take_quiz);
+        BTN_viewResults = findViewById(R.id.BTN_quiz_results);
+        BTN_suggestedCareers = findViewById(R.id.BTN_see_career_area_matches);
+        BTN_favouritesList = findViewById(R.id.BTN_favourites);
+
+        //On click go to personality quiz results
+        BTN_viewResults.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPersonalityQuizResults();
+            }
+        });
+
 
     }
+
+    /**
+     * Method to go to personality quiz results on click of view results button on home page.
+     */
+    private void viewPersonalityQuizResults() {
+        Intent viewResultsIntent = new Intent(this, PersonalityResultsDisplay.class);
+        startActivity(viewResultsIntent);
+    }
+
+
 }
