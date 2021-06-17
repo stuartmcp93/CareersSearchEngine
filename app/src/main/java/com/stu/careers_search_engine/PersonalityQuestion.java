00e19d76d23fe32f56e1d813e43ef17145b32a87
Stuart McPherson <stuartmcp93@gmail.com>
2021-06-17 21:55:50 +0100

    Added personality_quiz_results.xml layout.
    Made edits to perosnaity reuslts display UI and edited the quiz display UI.

--- app/src/main/java/com/stu/careers_search_engine/PersonalityQuestion.java
+++ app/src/main/java/com/stu/careers_search_engine/PersonalityQuestion.java
@@ -2,17 +2,36 @@ package com.stu.careers_search_engine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.util.Objects;
 
-public class PersonalityQuestion extends AppCompatActivity {
+public class PersonalityQuestion extends Activity {
+
+    ImageView IMG_home_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personality_question);
-        Objects.requireNonNull(getSupportActionBar()).hide();
-        //adding a commit
+
+        IMG_home_btn = findViewById(R.id.IMG_home_logo_quiz);
+
+        IMG_home_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnHome();
+            }
+        });
+
+    }
+
+    private void returnHome() {
+        Intent returnHome = new Intent(this, Home.class);
+        startActivity(returnHome);
     }
 }
--- app/src/main/java/com/stu/careers_search_engine/PersonalityResultsDisplay.java
+++ app/src/main/java/com/stu/careers_search_engine/PersonalityResultsDisplay.java
@@ -1,7 +1,13 @@
 package com.stu.careers_search_engine;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,57 +18,79 @@ import org.eazegraph.lib.models.PieModel;
 import java.util.Objects;
 
 public class PersonalityResultsDisplay extends AppCompatActivity {
-    TextView tvR, tvPython, tvCPP, tvJava;
+    TextView TV_con, TV_agreeableness, TV_neuroticism, TV_openness, TV_extraversion;
     PieChart pieChart;
+    ImageView IMG_home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personality_quiz_results);
-        Objects.requireNonNull(getSupportActionBar()).hide();
+        //Objects.requireNonNull(getSupportActionBar()).hide();
 
 
         // Link those objects with their respective
         // id's that we have given in .XML file
-        tvR = findViewById(R.id.tvR);
-        tvPython = findViewById(R.id.tvPython);
-        tvCPP = findViewById(R.id.tvCPP);
-        tvJava = findViewById(R.id.tvJava);
+        TV_con = findViewById(R.id.TV_conscientiousness);
+        TV_agreeableness = findViewById(R.id.TV_agreeableness);
+        TV_neuroticism = findViewById(R.id.TV_neuroticism);
+        TV_openness = findViewById(R.id.TV_openness_to_exp);
+        TV_extraversion = findViewById(R.id.TV_extraversion);
         pieChart = findViewById(R.id.piechart);
 
         // Creating a method setData()
         // to set the text in text view and pie chart
         setData();
+
+        IMG_home = findViewById(R.id.IMG_home_logo_quiz_res);
+
+        IMG_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnHome();
+            }
+        });
+    }
+
+    private void returnHome() {
+        Intent homeIntent = new Intent(this, Home.class);
+        startActivity(homeIntent);
     }
 
     private void setData()
     {
 
         // Set the percentage of language used
-        tvR.setText(Integer.toString(40));
-        tvPython.setText(Integer.toString(30));
-        tvCPP.setText(Integer.toString(5));
-        tvJava.setText(Integer.toString(25));
+        TV_con.setText(Integer.toString(40));
+        TV_agreeableness.setText(Integer.toString(30));
+        TV_neuroticism.setText(Integer.toString(5));
+        TV_openness.setText(Integer.toString(25));
+        TV_extraversion.setText(Integer.toString(35));
 
         // Set the data and color to the pie chart
         pieChart.addPieSlice(
                 new PieModel(
                         "Conscientiousness",
-                        Integer.parseInt(tvR.getText().toString()),
+                        Integer.parseInt(TV_con.getText().toString()),
                         Color.parseColor("#FFA726")));
         pieChart.addPieSlice(
                 new PieModel(
-                        "Python",
-                        Integer.parseInt(tvPython.getText().toString()),
+                        "Agreeableness",
+                        Integer.parseInt(TV_agreeableness.getText().toString()),
                         Color.parseColor("#66BB6A")));
         pieChart.addPieSlice(
                 new PieModel(
-                        "C++",
-                        Integer.parseInt(tvCPP.getText().toString()),
+                        "Neuroticism",
+                        Integer.parseInt(TV_neuroticism.getText().toString()),
                         Color.parseColor("#EF5350")));
         pieChart.addPieSlice(
                 new PieModel(
-                        "Java",
-                        Integer.parseInt(tvJava.getText().toString()),
+                        "Openness to experience",
+                        Integer.parseInt(TV_openness.getText().toString()),
+                        Color.parseColor("#EF5350")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Extraversion",
+                        Integer.parseInt(TV_extraversion.getText().toString()),
                         Color.parseColor("#29B6F6")));
 
         // To animate the pie chart
